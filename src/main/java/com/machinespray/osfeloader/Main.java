6a61a74d5086eff41379df788e0925d780e79761
machinespray <machinespray1@gmail.com>
2019-03-17 14:16:34 -0400

    Update to v163

--- src/main/java/com/machinespray/osfeloader/Main.java
+++ src/main/java/com/machinespray/osfeloader/Main.java
@@ -8,8 +8,8 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 public class Main {
-	public static final int revision = 1;
-	public static final String targetVersion = "v158";
+	public static final int revision = 0;
+	public static final String targetVersion = "v163";
 	//TODO(there's definitly a better way to do this logistically)
 	public static final XmlHolder[] simpleSolutions = initXml(new String[]{"Spells.xml", "Artifacts.xml", "Structures.xml", "Heroes.xml"});
 	private static DialogDisplaySpells dialog;
